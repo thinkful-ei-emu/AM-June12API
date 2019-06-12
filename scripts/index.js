d552ff4b8a55ffc07e5d3e7832a7ff6c0518f88e
Martha Plantz <maplantz89@gmail.com>
2019-06-12 09:18:28 -0700

    used our getItem from the api.js to store our items in the DOM

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,16 @@
 'use strict';
-/* global shoppingList, store, Item */
+/* global shoppingList, store, Item, api */
 // eslint-disable-next-line no-unused-vars
 $(document).ready(function() {
   shoppingList.bindEventListeners();
   shoppingList.render();
+
+  api.getItems()
+    .then(res => res.json())
+    .then((items) => {
+      items.forEach((item) => store.addItem(item));
+      shoppingList.render();
+    });
 });
 
 store.items.push(Item.create('apples'));
