78e6806c63e7f60aa9bfffee261787913b28fd68
Adrienne Smith <agsmith046@gmail.com>
2019-06-12 13:22:01 -0500

    updated store methods findAndUpdate

--- scripts/store.js
+++ scripts/store.js
@@ -10,23 +10,8 @@ const store = (function(){
     return this.items.find(item => item.id === id);
   };
 
-  const findAndToggleChecked = function(id) {
-    const item = this.findById(id);
-    item.checked = !item.checked;
-  };
-
   const findAndDelete = function(id) {
     this.items = this.items.filter(item => item.id !== id);
-  };
-
-  const findAndUpdateName = function(id, name) {
-    try {
-      Item.validateName(name);
-      const item = this.findById(id);
-      item.name = name;
-    } catch(e) {
-      console.log('Cannot update name: ' + e.message);
-    }
   };
 
   const toggleCheckedFilter = function() {
@@ -42,19 +27,22 @@ const store = (function(){
     this.searchTerm = term;
   };
 
+  const findAndUpdate = function(id, newData){
+    let foundItem = store.findById(id);
+    Object.assign(foundItem, newData);
+  }
+
   return {
     items: [],
     hideCheckedItems: false,
     searchTerm: '',
-
     addItem,
     findById,
-    findAndToggleChecked,
     findAndDelete,
-    findAndUpdateName,
     toggleCheckedFilter,
     setSearchTerm,
     setItemIsEditing,
+    findAndUpdate
   };
   
 }());
